84e7bd2e15e5f526af584d73c63015d413a53e7f
YuriyKabanenko <kabanpuxluy@gmail.com>
2023-02-27 23:03:46 +0200

    Add delete method to delete article by id

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,25 @@ app.post('', async (req, res) => {
     const data = await client.query('INSERT INTO articles (title, author, content, date) VALUES ($1, $2, $3, $4)',
       [title, author, content, date]);
 
-      res.status(201).json({ message: `Article with id ${data.fields} created successfully` });
+    res.status(201).json({ message: `Article with id ${data.fields} created successfully` });
+    client.release();
+  } catch (exception) {
+    console.error(exception);
+    res.send('Error ' + exception);
+  }
+});
+
+app.delete('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const client = await pool.connect();
+    const data = await client.query('DELETE FROM articles WHERE id = $1', [id]);
+    if (data.rowCount === 1) {
+      res.status(204).json();
+    } else {
+      res.status(404).json({ message: 'Article not found' });
+    }
+
   } catch (exception) {
     console.error(exception);
     res.send('Error ' + exception);

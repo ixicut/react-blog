801abd1469b6c55dbff9b4f8b6d757f3c482897f
Volodia <parikripakkabak@gmail.com>
2023-02-28 15:34:09 +0200

    add side-menu

--- frontend/src/components/SideMenu/SideMenu.jsx
+++ frontend/src/components/SideMenu/SideMenu.jsx
@@ -3,8 +3,8 @@ import './SideMenu.css';
 const SideMenu = () => {
     return (
         <aside>
-            <h2>Categories</h2>
-            <ul>
+            <h2 class = "side-menu-title">Categories</h2>
+            <ul>    
                 <li><a href="#">Category 1</a></li>
                 <li><a href="#">Category 2</a></li>
                 <li><a href="#">Category 3</a></li>

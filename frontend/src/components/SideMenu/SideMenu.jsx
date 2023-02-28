d5e01cda43199195e66dec34e5d702e1020a8e8b
Volodia <parikripakkabak@gmail.com>
2023-02-28 17:48:02 +0200

    fix side-menu

--- frontend/src/components/SideMenu/SideMenu.jsx
+++ frontend/src/components/SideMenu/SideMenu.jsx
@@ -4,7 +4,7 @@ const SideMenu = () => {
     return (
         <aside>
             <h2 class = "side-menu-title">Categories</h2>
-            <ul>    
+            <ul class = "side-ul">    
                 <li><a class = "cat-link" href="#">Category 1</a></li>
                 <li><a class = "cat-link" href="#">Category 2</a></li>
                 <li><a class = "cat-link" href="#">Category 3</a></li>

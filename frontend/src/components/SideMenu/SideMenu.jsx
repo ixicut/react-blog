63501e0a72db3b41667d08f785093474160eaf38
Volodia <parikripakkabak@gmail.com>
2023-02-28 15:43:57 +0200

    fix side menu animation and navbar logo

--- frontend/src/components/SideMenu/SideMenu.jsx
+++ frontend/src/components/SideMenu/SideMenu.jsx
@@ -5,10 +5,10 @@ const SideMenu = () => {
         <aside>
             <h2 class = "side-menu-title">Categories</h2>
             <ul>    
-                <li><a href="#">Category 1</a></li>
-                <li><a href="#">Category 2</a></li>
-                <li><a href="#">Category 3</a></li>
-                <li><a href="#">Category 4</a></li>
+                <li><a class = "cat-link" href="#">Category 1</a></li>
+                <li><a class = "cat-link" href="#">Category 2</a></li>
+                <li><a class = "cat-link" href="#">Category 3</a></li>
+                <li><a class = "cat-link" href="#">Category 4</a></li>
             </ul>
         </aside>
     );

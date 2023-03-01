957c44b208b3de760da80b6021659e0ee34e274e
YuriyKabanenko <kabanpuxluy@gmail.com>
2023-03-01 20:08:13 +0200

    Add to service (addArticle, deleteArticle, updateArticle) methods

--- frontend/src/service/Service.js
+++ frontend/src/service/Service.js
@@ -1,8 +1,38 @@
 export async function retrieveArticle() {
-    const response = await fetch('http://localhost:3001', {
-        method: 'GET'
-      });
-      const body = await response.text();
-      const article = body === "" ? {} : JSON.parse(body);
-      return article;
+  const response = await fetch('http://localhost:3001', {
+    method: 'GET'
+  });
+  const body = await response.text();
+  const articles = body === "" ? {} : JSON.parse(body);
+  return articles;
+}
+
+export async function addArticle(article) {
+  const response = await fetch(`http://localhost:3001`, {
+    method: 'POST',
+    body: article
+  });
+
+  const body = await response.text();
+  const message = body === "" ? {} : JSON.parse(body);
+  return message;
+}
+
+export async function deleteArticle(id) {
+  const response = await fetch(`http://localhost:3001/${id}`, {
+    method: 'DELETE'
+  });
+  const body = await response.text();
+  const message = body === "" ? {} : JSON.parse(body);
+  return message;
+}
+
+export async function updateArticle(id, article) {
+  const response = await fetch(`http://localhost:3001/${id}`, {
+    method: 'PUT',
+    body: article
+  });
+  const body = await response.text();
+  const message = body === "" ? {} : JSON.parse(body);
+  return message;
 }

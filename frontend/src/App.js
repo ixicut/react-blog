9b6d49ef74592a3ed2205ad2d99c42b6d7395782
YuriyKabanenko <kabanpuxluy@gmail.com>
2023-03-02 22:13:25 +0200

    Fixed bug with routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,7 @@ import { retrieveArticle } from './service/Service';
 import { useEffect, useState } from 'react';
 import {
   Route,
-  Router,
   Routes,
-  Link
 } from 'react-router-dom'
 import AddArticle from './components/AddArticle/AddArticle';
 import Main from './components/Main/Main';
@@ -23,7 +21,11 @@ function App() {
   }, [articles]);
 
   return (
-    <Main articles={articles} />
+    <Routes>
+      <Route exact path="/" element={<Main articles={articles} />} />
+      <Route path="/add-article" element={<AddArticle/>}/>
+    </Routes>
+
   );
 }
 

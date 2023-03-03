5af1b28667f66b3f2a7f872fce679503326c5be6
YuriyKabanenko <kabanpuxluy@gmail.com>
2023-03-03 21:41:55 +0200

    Add posibility entering data to input fields

--- frontend/src/components/AddArticle/AddArticle.jsx
+++ frontend/src/components/AddArticle/AddArticle.jsx
@@ -1,19 +1,25 @@
 import "./AddArticle.css"
+import { useState } from "react";
 
 const AddArticle = () => {
+
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [content, setContent] = useState('');
+
     return (
         <form >
             <label>
                 Заголовок:
-                <input type="text" />
+                <input type="text" value={title} onChange={e => { setTitle(e.target.value) }} />
             </label>
             <label>
                 Автор:
-                <input type="text" />
+                <input type="text" value={author} onChange={e => { setAuthor(e.target.value) }} />
             </label>
             <label>
                 Контент:
-                <textarea />
+                <textarea value={content} onChange={e => { setContent(e.target.value) }} />
             </label>
             <button type="submit">Створити статтю</button>
         </form>

a84bd646f7df0da2f306848a496e09532b47d3a5
Volodia <parikripakkabak@gmail.com>
2023-03-04 18:27:20 +0200

    replace navbar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,21 +1,35 @@
-import SearchBar from '../SearchBar/SearchBar';
+import '../../bootstrap.min.css';
 import './NavBar.css';
+import SearchBar from '../SearchBar/SearchBar';
 
 const NavBar = () => {
     return (
-        <nav class="navbar">
-            <ul class="navbar-nav">
-                <li>
-                <a class="logo nav-link" href='/'>
-                <img src='blog.svg' class = "logo-embed" alt="logo"></img>
+        <nav class="navbar navbar-expand-lg sticky-top navbar-light navbar-theme">
+        <div class="container-fluid">
+            <a class="navbar-brand logo" href='/'>
+                <img src='blog.svg' class="logo-embed" alt="logo"></img>
                 React Blog
-                </a>
-                </li>
-                <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
-            </ul>
-            <SearchBar></SearchBar>
-        </nav>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+                <SearchBar></SearchBar>
+            </div>
+        </div>
+    </nav>
     );
 }
 

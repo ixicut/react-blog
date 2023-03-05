832af98fa941f89a053a09ededa59f6b2144a709
Volodia <parikripakkabak@gmail.com>
2023-03-05 15:24:03 +0200

    added empty placer

--- frontend/src/components/ArticleList/ArticleList.jsx
+++ frontend/src/components/ArticleList/ArticleList.jsx
@@ -1,21 +1,41 @@
 import Article from "../Article/Article";
+import EmptyPlacer from "../EmptyPlacer/EmptyPlace";
 import '../../bootstrap.min.css';
 import './ArticleList.css';
+
+function arrayIsEmpty(array) {
+    if (!Array.isArray(array)) {
+        return false;
+    }
+
+    if (array.length === 0) {
+        return true;
+    }
+
+    return false;
+}
 
 const ArticleList = ({ articles }) => {
     return (
         <main class="col-md-10 ms-sm-auto col-lg-10 px-md-4">
             <div
                 class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom border-success">
-                <div class = "black-white-shimmer-text"><h1 class="h1">Dashboard</h1></div>
+                <div class="black-white-shimmer-text"><h1 class="h1">Dashboard</h1></div>
                 <a class="btn btn-success" href="/add-article">Create Article</a>
             </div>
 
             <div class="row">
                 {
-                    articles.map(article => (
-                        <Article id={article.id} title={article.title} author={article.author} content={article.content} date={article.date}/>
-                    ))
+                    arrayIsEmpty(articles) ? 
+                    <EmptyPlacer caption={"There is no blogs"}/>: 
+                            articles.map(article => (
+                            <Article id={article.id}
+                                title={article.title}
+                                author={article.author}
+                                content={article.content}
+                                date={article.date}
+                            />
+                        ))
                 }
             </div>
         </main>
